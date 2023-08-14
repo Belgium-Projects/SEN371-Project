e5d8a9830310416bccf79e432f68acc63c2d752f
MR-coder246 <112022997+MR-coder246@users.noreply.github.com>
2023-08-14 14:50:41 +0200

    Help!!!

--- SEN371-Project/SEN371-Project/LoginForm.Designer.cs
+++ SEN371-Project/SEN371-Project/LoginForm.Designer.cs
@@ -29,79 +29,22 @@ namespace SEN371_Project
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCopy = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.lblPass = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.lblUser = new System.Windows.Forms.Label();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
-            this.lblCopy = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.lblPass);
-            this.panel1.Controls.Add(this.txtPassword);
-            this.panel1.Controls.Add(this.lblUser);
-            this.panel1.Controls.Add(this.txtUsername);
-            this.panel1.Controls.Add(this.btnLogin);
-            this.panel1.Location = new System.Drawing.Point(309, 132);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(295, 323);
-            this.panel1.TabIndex = 6;
-            // 
-            // lblPass
-            // 
-            this.lblPass.AutoSize = true;
-            this.lblPass.Location = new System.Drawing.Point(120, 193);
-            this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(59, 15);
-            this.lblPass.TabIndex = 4;
-            this.lblPass.Text = "Password";
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(58, 211);
-            this.txtPassword.Multiline = true;
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(184, 38);
-            this.txtPassword.TabIndex = 3;
-            // 
-            // lblUser
-            // 
-            this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(117, 105);
-            this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(62, 15);
-            this.lblUser.TabIndex = 2;
-            this.lblUser.Text = "Username";
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.Location = new System.Drawing.Point(58, 123);
-            this.txtUsername.Multiline = true;
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(184, 40);
-            this.txtUsername.TabIndex = 1;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.BackColor = System.Drawing.Color.Lime;
-            this.btnLogin.Location = new System.Drawing.Point(92, 269);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(124, 51);
-            this.btnLogin.TabIndex = 0;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // lblCopy
             // 
@@ -114,41 +57,118 @@ namespace SEN371_Project
             // pictureBox2
             // 
             this.pictureBox2.Image = global::SEN371_Project.Properties.Resources.Wall_UnSat__1_;
-            this.pictureBox2.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox2.Location = new System.Drawing.Point(-15, -5);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(901, 537);
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // panel2
             // 
-            this.pictureBox1.Image = global::SEN371_Project.Properties.Resources.Premier_Service_Solutions_540_110_px_Final;
-            this.pictureBox1.Location = new System.Drawing.Point(277, 29);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(370, 62);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.lblPass);
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Controls.Add(this.lblUser);
+            this.panel2.Controls.Add(this.txtUsername);
+            this.panel2.Location = new System.Drawing.Point(234, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(438, 260);
+            this.panel2.TabIndex = 12;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(96, 44);
+            this.label4.Location = new System.Drawing.Point(165, 53);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(93, 19);
-            this.label4.TabIndex = 7;
+            this.label4.TabIndex = 14;
             this.label4.Text = "Please Login?";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(73, 12);
+            this.label2.Location = new System.Drawing.Point(142, 21);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 22);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 13;
             this.label2.Text = "Welcome Back!!";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(187, 174);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(59, 15);
+            this.lblPass.TabIndex = 12;
+            this.lblPass.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 192);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(184, 38);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(184, 86);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(62, 15);
+            this.lblUser.TabIndex = 10;
+            this.lblUser.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(125, 104);
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(184, 40);
+            this.txtUsername.TabIndex = 9;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Lime;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
+            this.btnLogin.Location = new System.Drawing.Point(234, 327);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(222, 76);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Lime;
+            this.btnRegister.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.Black;
+            this.btnRegister.Location = new System.Drawing.Point(449, 327);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(223, 76);
+            this.btnRegister.TabIndex = 14;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SEN371_Project.Properties.Resources.Premier_Service_Solutions_540_110_px_Final;
+            this.pictureBox1.Location = new System.Drawing.Point(254, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(381, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
             // 
             // frmLogin
             // 
@@ -157,15 +177,17 @@ namespace SEN371_Project
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(898, 491);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.lblCopy);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox2);
             this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmLogin";
             this.Text = "Login Page";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -174,15 +196,16 @@ namespace SEN371_Project
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCopy;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblPass;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.Label lblCopy;
+        private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label2;
     }
 }
--- SEN371-Project/SEN371-Project/dataHandler/Employee.cs
+++ SEN371-Project/SEN371-Project/dataHandler/Employee.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,22 +10,50 @@ namespace SEN371_Project.dataHandler
 {
     internal  class employee :dataLog
     {
+           
         string empName;
         string empId;
         string empSurname;
         string empRole;
         string empPhoneNumber;
+        string empUsername;
+        string empPassword;
+        private string HashedPassword;
 
-        public employee(string empName, string empId, string empSurname, string empRole)
+        public employee(string empName, string empId, string empSurname, string empRole, string empPhoneNumber,string empUsername, string empPassword)
         {
             this.empName = empName;
             this.empId = empId;
             this.empSurname = empSurname;
             this.empRole = empRole;
+            this.empPhoneNumber = empPhoneNumber;
+            this.empUsername = empUsername;
+            this.empPassword = empPassword;
+
         }
-        
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            if (username == Username && HashPassword(password, Salt) == HashedPassword)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // Helper method to hash the password with salt
+        private static string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] saltedPassword = Encoding.UTF8.GetBytes(password + salt);
+                byte[] hashBytes = sha256.ComputeHash(saltedPassword);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
         //Checks if employee can access function
-         bool canAcces(int empRole, string RoleNeeded)
+        bool canAcces(int empRole, string RoleNeeded)
         {
             return true;
         }
@@ -53,5 +82,7 @@ namespace SEN371_Project.dataHandler
         public string EmpId { get => empId; set => empId = value; }
         public string EmpSurname { get => empSurname; set => empSurname = value; }
         public string EmpRole { get => empRole; set => empRole = value; }
+        public string Username { get; private set; }
+        public string Salt { get; private set; }
     }
 }
