c60916ddba7bb7d3e4d5fd28a26cc96d1c8fbb71
DayDreamerDevs <rynoreyneke54@gmail.com>
2023-08-16 00:45:58 +0200

    Added all UI to manageForm and Handled formatRules

--- SEN371-Project/SEN371-Project/managementDeptForm.Designer.cs
+++ SEN371-Project/SEN371-Project/managementDeptForm.Designer.cs
@@ -29,15 +29,31 @@
         private void InitializeComponent()
         {
             this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.btnClient = new System.Windows.Forms.Button();
             this.btndepartment = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnClient = new System.Windows.Forms.Button();
-            this.btnWork = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlContent1 = new System.Windows.Forms.Panel();
+            this.btnViewReports = new System.Windows.Forms.Button();
+            this.btnViewEmployee = new System.Windows.Forms.Button();
+            this.btnViewServices = new System.Windows.Forms.Button();
+            this.btnPerformance = new System.Windows.Forms.Button();
+            this.lblContent2 = new System.Windows.Forms.Label();
+            this.lblContent1 = new System.Windows.Forms.Label();
+            this.pnlContent2 = new System.Windows.Forms.Panel();
+            this.pnlBR3 = new System.Windows.Forms.Panel();
+            this.pnlContent3 = new System.Windows.Forms.Panel();
+            this.pnlBR1 = new System.Windows.Forms.Panel();
+            this.pnlBR2 = new System.Windows.Forms.Panel();
+            this.lblLine1 = new System.Windows.Forms.Label();
             this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlContent1.SuspendLayout();
+            this.pnlContent2.SuspendLayout();
+            this.pnlBR3.SuspendLayout();
+            this.pnlBR1.SuspendLayout();
+            this.pnlBR2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlHeader
@@ -54,8 +70,28 @@
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
             this.pnlHeader.Name = "pnlHeader";
             this.pnlHeader.Padding = new System.Windows.Forms.Padding(3);
-            this.pnlHeader.Size = new System.Drawing.Size(1295, 124);
+            this.pnlHeader.Size = new System.Drawing.Size(1924, 124);
             this.pnlHeader.TabIndex = 0;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWork.Location = new System.Drawing.Point(1145, 37);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(138, 55);
+            this.btnWork.TabIndex = 4;
+            this.btnWork.Text = "Work";
+            this.btnWork.UseVisualStyleBackColor = true;
+            // 
+            // btnClient
+            // 
+            this.btnClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClient.Location = new System.Drawing.Point(968, 37);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(143, 55);
+            this.btnClient.TabIndex = 3;
+            this.btnClient.Text = "Client";
+            this.btnClient.UseVisualStyleBackColor = true;
             // 
             // btndepartment
             // 
@@ -90,39 +126,138 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // btnClient
+            // pnlContent1
             // 
-            this.btnClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClient.Location = new System.Drawing.Point(968, 37);
-            this.btnClient.Name = "btnClient";
-            this.btnClient.Size = new System.Drawing.Size(143, 55);
-            this.btnClient.TabIndex = 3;
-            this.btnClient.Text = "Client";
-            this.btnClient.UseVisualStyleBackColor = true;
+            this.pnlContent1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.pnlContent1.Controls.Add(this.lblLine1);
+            this.pnlContent1.Controls.Add(this.btnViewReports);
+            this.pnlContent1.Controls.Add(this.btnViewEmployee);
+            this.pnlContent1.Controls.Add(this.btnViewServices);
+            this.pnlContent1.Controls.Add(this.btnPerformance);
+            this.pnlContent1.Controls.Add(this.lblContent2);
+            this.pnlContent1.Controls.Add(this.lblContent1);
+            this.pnlContent1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent1.Location = new System.Drawing.Point(10, 10);
+            this.pnlContent1.Name = "pnlContent1";
+            this.pnlContent1.Size = new System.Drawing.Size(328, 710);
+            this.pnlContent1.TabIndex = 1;
             // 
-            // btnWork
+            // btnViewReports
             // 
-            this.btnWork.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWork.Location = new System.Drawing.Point(1145, 37);
-            this.btnWork.Name = "btnWork";
-            this.btnWork.Size = new System.Drawing.Size(138, 55);
-            this.btnWork.TabIndex = 4;
-            this.btnWork.Text = "Work";
-            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnViewReports.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewReports.Location = new System.Drawing.Point(29, 606);
+            this.btnViewReports.Name = "btnViewReports";
+            this.btnViewReports.Size = new System.Drawing.Size(270, 70);
+            this.btnViewReports.TabIndex = 4;
+            this.btnViewReports.Text = "View Reports";
+            this.btnViewReports.UseVisualStyleBackColor = true;
             // 
-            // panel1
+            // btnViewEmployee
             // 
-            this.panel1.Location = new System.Drawing.Point(31, 156);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(272, 339);
-            this.panel1.TabIndex = 1;
+            this.btnViewEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewEmployee.Location = new System.Drawing.Point(29, 474);
+            this.btnViewEmployee.Name = "btnViewEmployee";
+            this.btnViewEmployee.Size = new System.Drawing.Size(270, 70);
+            this.btnViewEmployee.TabIndex = 3;
+            this.btnViewEmployee.Text = "View Employee";
+            this.btnViewEmployee.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // btnViewServices
             // 
-            this.panel2.Location = new System.Drawing.Point(358, 156);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(858, 339);
-            this.panel2.TabIndex = 2;
+            this.btnViewServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewServices.Location = new System.Drawing.Point(29, 340);
+            this.btnViewServices.Name = "btnViewServices";
+            this.btnViewServices.Size = new System.Drawing.Size(270, 70);
+            this.btnViewServices.TabIndex = 3;
+            this.btnViewServices.Text = "View Services";
+            this.btnViewServices.UseVisualStyleBackColor = true;
+            // 
+            // btnPerformance
+            // 
+            this.btnPerformance.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPerformance.Location = new System.Drawing.Point(29, 205);
+            this.btnPerformance.Name = "btnPerformance";
+            this.btnPerformance.Size = new System.Drawing.Size(270, 70);
+            this.btnPerformance.TabIndex = 2;
+            this.btnPerformance.Text = "View Performance";
+            this.btnPerformance.UseVisualStyleBackColor = true;
+            // 
+            // lblContent2
+            // 
+            this.lblContent2.AutoSize = true;
+            this.lblContent2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContent2.Location = new System.Drawing.Point(117, 106);
+            this.lblContent2.Name = "lblContent2";
+            this.lblContent2.Size = new System.Drawing.Size(97, 29);
+            this.lblContent2.TabIndex = 1;
+            this.lblContent2.Text = "Options";
+            // 
+            // lblContent1
+            // 
+            this.lblContent1.AutoSize = true;
+            this.lblContent1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContent1.Location = new System.Drawing.Point(83, 52);
+            this.lblContent1.Name = "lblContent1";
+            this.lblContent1.Size = new System.Drawing.Size(164, 36);
+            this.lblContent1.TabIndex = 0;
+            this.lblContent1.Text = "Tony Hawk";
+            // 
+            // pnlContent2
+            // 
+            this.pnlContent2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(104)))), ((int)(((byte)(104)))));
+            this.pnlContent2.Controls.Add(this.pnlBR3);
+            this.pnlContent2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent2.Location = new System.Drawing.Point(10, 10);
+            this.pnlContent2.Name = "pnlContent2";
+            this.pnlContent2.Size = new System.Drawing.Size(1439, 780);
+            this.pnlContent2.TabIndex = 2;
+            // 
+            // pnlBR3
+            // 
+            this.pnlBR3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.pnlBR3.Controls.Add(this.pnlContent3);
+            this.pnlBR3.Location = new System.Drawing.Point(28, 205);
+            this.pnlBR3.Name = "pnlBR3";
+            this.pnlBR3.Padding = new System.Windows.Forms.Padding(10);
+            this.pnlBR3.Size = new System.Drawing.Size(1362, 548);
+            this.pnlBR3.TabIndex = 0;
+            // 
+            // pnlContent3
+            // 
+            this.pnlContent3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            this.pnlContent3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent3.Location = new System.Drawing.Point(10, 10);
+            this.pnlContent3.Name = "pnlContent3";
+            this.pnlContent3.Size = new System.Drawing.Size(1342, 528);
+            this.pnlContent3.TabIndex = 0;
+            // 
+            // pnlBR1
+            // 
+            this.pnlBR1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.pnlBR1.Controls.Add(this.pnlContent1);
+            this.pnlBR1.Location = new System.Drawing.Point(25, 150);
+            this.pnlBR1.Name = "pnlBR1";
+            this.pnlBR1.Padding = new System.Windows.Forms.Padding(10);
+            this.pnlBR1.Size = new System.Drawing.Size(348, 730);
+            this.pnlBR1.TabIndex = 5;
+            // 
+            // pnlBR2
+            // 
+            this.pnlBR2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.pnlBR2.Controls.Add(this.pnlContent2);
+            this.pnlBR2.Location = new System.Drawing.Point(426, 150);
+            this.pnlBR2.Name = "pnlBR2";
+            this.pnlBR2.Padding = new System.Windows.Forms.Padding(10);
+            this.pnlBR2.Size = new System.Drawing.Size(1459, 800);
+            this.pnlBR2.TabIndex = 6;
+            // 
+            // lblLine1
+            // 
+            this.lblLine1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lblLine1.Location = new System.Drawing.Point(78, 92);
+            this.lblLine1.Name = "lblLine1";
+            this.lblLine1.Size = new System.Drawing.Size(180, 4);
+            this.lblLine1.TabIndex = 5;
             // 
             // frmManagementDept
             // 
@@ -131,11 +266,10 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImage = global::SEN371_Project.Properties.Resources.Wall_UnSat__1_;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1295, 524);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1924, 1000);
             this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.pnlBR1);
+            this.Controls.Add(this.pnlBR2);
             this.DoubleBuffered = true;
             this.Name = "frmManagementDept";
             this.Text = "Management Department";
@@ -143,6 +277,12 @@
             this.Load += new System.EventHandler(this.frmManagementDept_Load);
             this.pnlHeader.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlContent1.ResumeLayout(false);
+            this.pnlContent1.PerformLayout();
+            this.pnlContent2.ResumeLayout(false);
+            this.pnlBR3.ResumeLayout(false);
+            this.pnlBR1.ResumeLayout(false);
+            this.pnlBR2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,7 +296,18 @@
         private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.Button btnWork;
         private System.Windows.Forms.Button btnClient;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlContent1;
+        private System.Windows.Forms.Panel pnlContent2;
+        private System.Windows.Forms.Label lblContent1;
+        private System.Windows.Forms.Label lblContent2;
+        private System.Windows.Forms.Button btnPerformance;
+        private System.Windows.Forms.Button btnViewEmployee;
+        private System.Windows.Forms.Button btnViewServices;
+        private System.Windows.Forms.Button btnViewReports;
+        private System.Windows.Forms.Panel pnlContent3;
+        private System.Windows.Forms.Panel pnlBR1;
+        private System.Windows.Forms.Panel pnlBR2;
+        private System.Windows.Forms.Panel pnlBR3;
+        private System.Windows.Forms.Label lblLine1;
     }
 }
--- SEN371-Project/SEN371-Project/managementDeptForm.cs
+++ SEN371-Project/SEN371-Project/managementDeptForm.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace SEN371_Project
 {
@@ -19,10 +21,121 @@ namespace SEN371_Project
 
         private void frmManagementDept_Load(object sender, EventArgs e)
         {
+            formattingRules();
+        }
+
+        private void formattingRules()
+        {
+            //Making Form dynamic in sizing elements
             Rectangle resolutionRect = System.Windows.Forms.Screen.FromControl(this).Bounds;
             if (this.Width >= resolutionRect.Width || this.Height >= resolutionRect.Height)
             {
                 this.WindowState = FormWindowState.Maximized;
+            }
+
+            //Variables for setting transparency
+            int maxIndex = 0;
+            int indexCount1 = 1;
+            int indexCount2 = 1;
+            int indexCount3 = 1;
+            bool stop1 = false;
+            bool stop2 = false;
+            bool stop3 = false;
+
+            //Setting Content Panel transparency/fixing flickering (Content Panel border Naming) *"pnlContent{Num}*
+            while (!stop1)
+            {
+                String ctlName = $"pnlContent{indexCount1}";
+                Panel panel = this.Controls.Find(ctlName, true).FirstOrDefault() as Panel;
+                if (panel != null)
+                {
+                    typeof(Panel).InvokeMember("DoubleBuffered",
+                    BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, panel, new object[] { true });
+
+                    SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+                    switch (indexCount1)
+                    {
+                        case 1:
+                            panel.BackColor = Color.FromArgb(150, 65, 65, 65);
+                            break;
+                        case 2:
+                            panel.BackColor = Color.FromArgb(150, 104, 104, 104);
+                            break;
+                        case 3:
+                            panel.BackColor = Color.FromArgb(190, 154, 154, 154);
+                            break;
+                        default:
+                            break;
+                    }
+                    indexCount1++;
+                }
+                else
+                {
+                    maxIndex = indexCount1;
+                    indexCount1 = 1;
+                    stop1 = true;
+                }
+            }
+
+            //Setting Panel borders transparency/fixing flickering (Panel border Naming) *"pnlBR{Num}*
+            while (!stop2)
+            {
+                String ctlName = $"pnlBR{indexCount2}";
+                Panel panel = this.Controls.Find(ctlName, true).FirstOrDefault() as Panel;
+                if (panel != null)
+                {
+                    typeof(Panel).InvokeMember("DoubleBuffered",
+                    BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, panel, new object[] { true });
+
+                    SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+                    switch (indexCount2)
+                    {
+                        case 1:
+                            panel.BackColor = Color.FromArgb(90, 217, 217, 217);
+                            break;
+                        case 2:
+                            panel.BackColor = Color.FromArgb(90, 217, 217, 217);
+                            break;
+                        case 3:
+                            panel.BackColor = Color.FromArgb(150, 217, 217, 217);
+                            break;
+                        default:
+                            break;
+                    }
+                    indexCount2++;
+                }
+                else
+                {
+                    indexCount2 = 1;
+                    stop2 = true;
+                }
+            }
+
+            //Setting Labels background transparent (Label naming) *"lblContent{num}"*
+            while (!stop3)
+            {
+                String ctlName = $"lblContent{indexCount3}";
+                Label label = this.Controls.Find(ctlName, true).FirstOrDefault() as Label;
+                if (label != null)
+                {
+                    typeof(Label).InvokeMember("DoubleBuffered",
+                    BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, label, new object[] { true });
+
+                    SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+                    label.BackColor = Color.FromArgb(0, 65, 65, 65);
+                    indexCount3++;
+                }
+                else
+                {
+                    indexCount3 = 1;
+                    stop3 = true;
+                }
             }
         }
     }
