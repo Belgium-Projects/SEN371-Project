cf40ff07f0b97bd6d023804fd2998b264897c767
MR-coder246 <112022997+MR-coder246@users.noreply.github.com>
2023-08-15 12:56:14 +0200

    happy

--- SEN371-Project/SEN371-Project/ApplicationForm.Designer.cs
+++ SEN371-Project/SEN371-Project/ApplicationForm.Designer.cs
@@ -31,22 +31,6 @@ namespace SEN371_Project
         {
             this.btnDepartment = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
-            this.pnlWork = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pnlDept = new System.Windows.Forms.Panel();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pnlWorkButtons = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnTransferCall = new System.Windows.Forms.Button();
-            this.btnAssign = new System.Windows.Forms.Button();
-            this.btnLogIssue = new System.Windows.Forms.Button();
-            this.btnViewIssue = new System.Windows.Forms.Button();
             this.btnClient = new System.Windows.Forms.Button();
             this.btnWork = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -59,11 +43,6 @@ namespace SEN371_Project
             this.btnJob = new System.Windows.Forms.Button();
             this.btnViewServices = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.pnlClientButtons = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.pnlClient = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.panel9 = new System.Windows.Forms.Panel();
@@ -85,17 +64,10 @@ namespace SEN371_Project
             this.button6 = new System.Windows.Forms.Button();
             this.label16 = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
-            this.pnlWork.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            this.pnlDept.SuspendLayout();
-            this.panel6.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.pnlWorkButtons.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.pnlDeptButton.SuspendLayout();
-            this.pnlClientButtons.SuspendLayout();
             this.pnlClient.SuspendLayout();
             this.panel9.SuspendLayout();
             this.SuspendLayout();
@@ -119,156 +91,6 @@ namespace SEN371_Project
             this.btnLogOut.Text = "Log Out";
             this.btnLogOut.UseVisualStyleBackColor = true;
             this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
-            // 
-            // pnlWork
-            // 
-            this.pnlWork.Controls.Add(this.panel3);
-            this.pnlWork.Controls.Add(this.label2);
-            this.pnlWork.Controls.Add(this.pictureBox4);
-            this.pnlWork.Controls.Add(this.pnlDept);
-            this.pnlWork.Location = new System.Drawing.Point(287, 80);
-            this.pnlWork.Name = "pnlWork";
-            this.pnlWork.Size = new System.Drawing.Size(676, 434);
-            this.pnlWork.TabIndex = 7;
-            // 
-            // panel3
-            // 
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Location = new System.Drawing.Point(18, 144);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(587, 294);
-            this.panel3.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(257, 94);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Location = new System.Drawing.Point(207, 15);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(143, 58);
-            this.pictureBox4.TabIndex = 1;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pnlDept
-            // 
-            this.pnlDept.Controls.Add(this.label12);
-            this.pnlDept.Controls.Add(this.label10);
-            this.pnlDept.Controls.Add(this.panel6);
-            this.pnlDept.Location = new System.Drawing.Point(18, 0);
-            this.pnlDept.Name = "pnlDept";
-            this.pnlDept.Size = new System.Drawing.Size(744, 507);
-            this.pnlDept.TabIndex = 31;
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(227, 69);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(228, 15);
-            this.label12.TabIndex = 10;
-            this.label12.Text = "Here you can update your account details";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(167, 25);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(367, 27);
-            this.label10.TabIndex = 9;
-            this.label10.Text = "Welcome to your Client portal page!";
-            // 
-            // panel6
-            // 
-            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel6.Controls.Add(this.dataGridView1);
-            this.panel6.Controls.Add(this.button2);
-            this.panel6.Location = new System.Drawing.Point(52, 100);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(674, 381);
-            this.panel6.TabIndex = 6;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(670, 377);
-            this.dataGridView1.TabIndex = 4;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(393, 399);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 27);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // pnlWorkButtons
-            // 
-            this.pnlWorkButtons.Controls.Add(this.label1);
-            this.pnlWorkButtons.Controls.Add(this.btnTransferCall);
-            this.pnlWorkButtons.Controls.Add(this.btnAssign);
-            this.pnlWorkButtons.Controls.Add(this.btnLogIssue);
-            this.pnlWorkButtons.Controls.Add(this.btnViewIssue);
-            this.pnlWorkButtons.Location = new System.Drawing.Point(12, 81);
-            this.pnlWorkButtons.Name = "pnlWorkButtons";
-            this.pnlWorkButtons.Size = new System.Drawing.Size(230, 341);
-            this.pnlWorkButtons.TabIndex = 8;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 15);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "label1";
-            // 
-            // btnTransferCall
-            // 
-            this.btnTransferCall.Location = new System.Drawing.Point(19, 295);
-            this.btnTransferCall.Name = "btnTransferCall";
-            this.btnTransferCall.Size = new System.Drawing.Size(190, 36);
-            this.btnTransferCall.TabIndex = 14;
-            this.btnTransferCall.Text = "Transfer Call";
-            this.btnTransferCall.UseVisualStyleBackColor = true;
-            // 
-            // btnAssign
-            // 
-            this.btnAssign.Location = new System.Drawing.Point(19, 227);
-            this.btnAssign.Name = "btnAssign";
-            this.btnAssign.Size = new System.Drawing.Size(190, 39);
-            this.btnAssign.TabIndex = 13;
-            this.btnAssign.Text = "Assign Job";
-            this.btnAssign.UseVisualStyleBackColor = true;
-            // 
-            // btnLogIssue
-            // 
-            this.btnLogIssue.Location = new System.Drawing.Point(19, 162);
-            this.btnLogIssue.Name = "btnLogIssue";
-            this.btnLogIssue.Size = new System.Drawing.Size(190, 42);
-            this.btnLogIssue.TabIndex = 12;
-            this.btnLogIssue.Text = "Log Issue";
-            this.btnLogIssue.UseVisualStyleBackColor = true;
-            // 
-            // btnViewIssue
-            // 
-            this.btnViewIssue.Location = new System.Drawing.Point(19, 94);
-            this.btnViewIssue.Name = "btnViewIssue";
-            this.btnViewIssue.Size = new System.Drawing.Size(190, 40);
-            this.btnViewIssue.TabIndex = 11;
-            this.btnViewIssue.Text = "View Issues";
-            this.btnViewIssue.UseVisualStyleBackColor = true;
             // 
             // btnClient
             // 
@@ -330,7 +152,7 @@ namespace SEN371_Project
             this.pnlDeptButton.Controls.Add(this.button1);
             this.pnlDeptButton.Location = new System.Drawing.Point(12, 80);
             this.pnlDeptButton.Name = "pnlDeptButton";
-            this.pnlDeptButton.Size = new System.Drawing.Size(209, 222);
+            this.pnlDeptButton.Size = new System.Drawing.Size(225, 379);
             this.pnlDeptButton.TabIndex = 32;
             // 
             // btnCall
@@ -388,53 +210,6 @@ namespace SEN371_Project
             this.button1.TabIndex = 7;
             this.button1.Text = "Log Issue";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // pnlClientButtons
-            // 
-            this.pnlClientButtons.Controls.Add(this.label3);
-            this.pnlClientButtons.Controls.Add(this.button5);
-            this.pnlClientButtons.Controls.Add(this.button4);
-            this.pnlClientButtons.Controls.Add(this.button3);
-            this.pnlClientButtons.Location = new System.Drawing.Point(14, 81);
-            this.pnlClientButtons.Name = "pnlClientButtons";
-            this.pnlClientButtons.Size = new System.Drawing.Size(207, 266);
-            this.pnlClientButtons.TabIndex = 34;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(72, 13);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 15);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Clients";
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(32, 206);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(136, 35);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "Delete Account";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(32, 77);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(136, 35);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "View Accounts";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(32, 144);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 35);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Services";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // pnlClient
             // 
@@ -634,13 +409,10 @@ namespace SEN371_Project
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1028, 670);
-            this.Controls.Add(this.pnlClientButtons);
             this.Controls.Add(this.pnlClient);
             this.Controls.Add(this.pnlDeptButton);
             this.Controls.Add(this.btnWork);
             this.Controls.Add(this.btnClient);
-            this.Controls.Add(this.pnlWorkButtons);
-            this.Controls.Add(this.pnlWork);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnDepartment);
             this.Controls.Add(this.pictureBox3);
@@ -650,22 +422,11 @@ namespace SEN371_Project
             this.Name = "frmApplication";
             this.Text = "Application";
             this.Load += new System.EventHandler(this.frmManagement_Load);
-            this.pnlWork.ResumeLayout(false);
-            this.pnlWork.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            this.pnlDept.ResumeLayout(false);
-            this.pnlDept.PerformLayout();
-            this.panel6.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.pnlWorkButtons.ResumeLayout(false);
-            this.pnlWorkButtons.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.pnlDeptButton.ResumeLayout(false);
             this.pnlDeptButton.PerformLayout();
-            this.pnlClientButtons.ResumeLayout(false);
-            this.pnlClientButtons.PerformLayout();
             this.pnlClient.ResumeLayout(false);
             this.pnlClient.PerformLayout();
             this.panel9.ResumeLayout(false);
@@ -681,24 +442,8 @@ namespace SEN371_Project
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button btnDepartment;
         private System.Windows.Forms.Button btnLogOut;
-        private System.Windows.Forms.Panel pnlWork;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Panel pnlWorkButtons;
         private System.Windows.Forms.Button btnClient;
         private System.Windows.Forms.Button btnWork;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnTransferCall;
-        private System.Windows.Forms.Button btnAssign;
-        private System.Windows.Forms.Button btnLogIssue;
-        private System.Windows.Forms.Button btnViewIssue;
-        private System.Windows.Forms.Panel pnlDept;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel pnlDeptButton;
         private System.Windows.Forms.Button btnCall;
         private System.Windows.Forms.Label label13;
@@ -706,11 +451,6 @@ namespace SEN371_Project
         private System.Windows.Forms.Button btnJob;
         private System.Windows.Forms.Button btnViewServices;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel pnlClientButtons;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Panel pnlClient;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel9;
