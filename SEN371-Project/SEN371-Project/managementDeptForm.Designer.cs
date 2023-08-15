f02b95acf85a55e22c538d5caa1689e382482861
DayDreamerDevs <rynoreyneke54@gmail.com>
2023-08-15 20:51:17 +0200

    Added basic elements to managementForm

--- SEN371-Project/SEN371-Project/managementDeptForm.Designer.cs
+++ SEN371-Project/SEN371-Project/managementDeptForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SEN371_Project
 {
-    partial class managementDeptForm
+    partial class frmManagementDept
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.btndepartment = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClient = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.AutoSize = true;
+            this.pnlHeader.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(90)))), ((int)(((byte)(112)))));
+            this.pnlHeader.Controls.Add(this.btnWork);
+            this.pnlHeader.Controls.Add(this.btnClient);
+            this.pnlHeader.Controls.Add(this.btndepartment);
+            this.pnlHeader.Controls.Add(this.btnHome);
+            this.pnlHeader.Controls.Add(this.pictureBox1);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Padding = new System.Windows.Forms.Padding(3);
+            this.pnlHeader.Size = new System.Drawing.Size(1295, 124);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // btndepartment
+            // 
+            this.btndepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndepartment.Location = new System.Drawing.Point(744, 37);
+            this.btndepartment.Name = "btndepartment";
+            this.btndepartment.Size = new System.Drawing.Size(192, 55);
+            this.btndepartment.TabIndex = 2;
+            this.btndepartment.Text = "Department";
+            this.btndepartment.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.Location = new System.Drawing.Point(556, 37);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(152, 55);
+            this.btnHome.TabIndex = 1;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(154)))), ((int)(((byte)(154)))));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::SEN371_Project.Properties.Resources.Premier_Service_Solutions_540_110_px_Final;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(502, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClient
+            // 
+            this.btnClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClient.Location = new System.Drawing.Point(968, 37);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(143, 55);
+            this.btnClient.TabIndex = 3;
+            this.btnClient.Text = "Client";
+            this.btnClient.UseVisualStyleBackColor = true;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWork.Location = new System.Drawing.Point(1145, 37);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(138, 55);
+            this.btnWork.TabIndex = 4;
+            this.btnWork.Text = "Work";
+            this.btnWork.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(31, 156);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 339);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(358, 156);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(858, 339);
+            this.panel2.TabIndex = 2;
+            // 
+            // frmManagementDept
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "managementDeptForm";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::SEN371_Project.Properties.Resources.Wall_UnSat__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1295, 524);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlHeader);
+            this.DoubleBuffered = true;
+            this.Name = "frmManagementDept";
+            this.Text = "Management Department";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmManagementDept_Load);
+            this.pnlHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btndepartment;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Button btnClient;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
